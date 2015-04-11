466b5d54161c9dbb73e7e71337024dbefc631591
Michael Aufreiter <michael@substance.io>
2015-04-11 10:19:59 +0200

    Upgrade to new document format.

--- models/document.js
+++ models/document.js
@@ -52,7 +52,7 @@ documentSchema.statics.createEmpty = function(cb) {
 	var emptyDocJson = {
     "_id": docId,
     "id": docId,
-    "_schema": [
+    "schema": [
       "substance-interview",
       "0.1.0"
     ],
@@ -65,10 +65,12 @@ documentSchema.statics.createEmpty = function(cb) {
         ],
         "guid": docId,
         "creator": "",
-        "title": "New Composer Document!",
+        "title": "Composer Test with multi-annotation",
         "abstract": "Test",
         "created_at": "2015-03-04T10:56:18.229Z",
         "updated_at": "2015-03-04T10:56:47.425Z",
+        "interview_subject_name": "Please enter interview subject name.",
+        "interview_subject_bio": "Please enter interview subject bio.",
         "published_on": "2015-03-04T10:56:18.230Z"
       },
       "content": {
@@ -76,7 +78,10 @@ documentSchema.statics.createEmpty = function(cb) {
         "id": "content",
         "nodes": [
           "text_1",
-          "text_2"
+          "text_2",
+          "text_3",
+          "text_4",
+          "text_5"
         ]
       },
       "text_1": {
@@ -84,7 +89,7 @@ documentSchema.statics.createEmpty = function(cb) {
         "id": "text_1",
         "content": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis scelerisque ligula. Proin tristique ligula id magna finibus rhoncus. Quisque dictum viverra sapien, vel elementum metus condimentum nec. Donec ac tellus nunc. Nullam fermentum pharetra justo, accumsan tristique quam tempus a. Quisque vitae luctus velit. Praesent lacinia enim ex, sed pulvinar neque dictum ultricies. Sed est metus, bibendum sed suscipit ut, cursus ut mi. Pellentesque sagittis mi nisi, eu blandit metus congue id. Pellentesque eget magna porta, rutrum odio et, commodo lacus. Sed vitae vehicula ante. Quisque suscipit iaculis est, vitae aliquet lacus dictum ut. Nulla enim dolor, pulvinar at odio vitae, sollicitudin eleifend ex. Maecenas eget ligula eget sem efficitur consectetur nec vel sem. In massa mauris, consequat vitae enim eget, vehicula aliquet turpis.'
       },
-      
+
       "text_2": {
         "type": "text",
         "id": "text_2",
@@ -108,29 +113,30 @@ documentSchema.statics.createEmpty = function(cb) {
       "subject_reference_1": {
         "id": "subject_reference_1",
         "type": "subject_reference",
-        "path": [
-          "text_2",
-          "content"
-        ],
-        "target": ["54bae4cda868bc6fab4bcd0e", "54bae99ca868bc3ec7fb5ad8"], // these are external objects
-        "range": [
-          99,
-          122
-        ]
+        "container": "content",
+        "startPath": ["text_2", "content"],
+        "startOffset": 100,
+        "endPath": ["text_4", "content"],
+        "endOffset": 40,
+        "target": ["54bae4cda868bc6fab4bcd0e", "54bae99ca868bc3ec7fb5ad8"]
       },
 
-      "subject_reference_2": {
-        "id": "subject_reference_2",
-        "type": "subject_reference",
-        "path": [
-          "text_2",
-          "content"
-        ],
-        "target": ["54bae4d0a868bc6fab4bcd16", "54bae99ca868bc3ec7fb5ad8"], // these are external objects
-        "range": [
-          614,
-          628
-        ]
+      "text_3": {
+        "type": "text",
+        "id": "text_3",
+        "content": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis scelerisque ligula. Proin tristique ligula id magna finibus rhoncus. Quisque dictum viverra sapien, vel elementum metus condimentum nec. Donec ac tellus nunc. Nullam fermentum pharetra justo, accumsan tristique quam tempus a. Quisque vitae luctus velit. Praesent lacinia enim ex, sed pulvinar neque dictum ultricies. Sed est metus, bibendum sed suscipit ut, cursus ut mi. Pellentesque sagittis mi nisi, eu blandit metus congue id. Pellentesque eget magna porta, rutrum odio et, commodo lacus. Sed vitae vehicula ante. Quisque suscipit iaculis est, vitae aliquet lacus dictum ut. Nulla enim dolor, pulvinar at odio vitae, sollicitudin eleifend ex. Maecenas eget ligula eget sem efficitur consectetur nec vel sem. In massa mauris, consequat vitae enim eget, vehicula aliquet turpis.'
+      },
+
+      "text_4": {
+        "type": "text",
+        "id": "text_4",
+        "content": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis scelerisque ligula. Proin tristique ligula id magna finibus rhoncus. Quisque dictum viverra sapien, vel elementum metus condimentum nec. Donec ac tellus nunc. Nullam fermentum pharetra justo, accumsan tristique quam tempus a. Quisque vitae luctus velit. Praesent lacinia enim ex, sed pulvinar neque dictum ultricies. Sed est metus, bibendum sed suscipit ut, cursus ut mi. Pellentesque sagittis mi nisi, eu blandit metus congue id. Pellentesque eget magna porta, rutrum odio et, commodo lacus. Sed vitae vehicula ante. Quisque suscipit iaculis est, vitae aliquet lacus dictum ut. Nulla enim dolor, pulvinar at odio vitae, sollicitudin eleifend ex. Maecenas eget ligula eget sem efficitur consectetur nec vel sem. In massa mauris, consequat vitae enim eget, vehicula aliquet turpis.'
+      },
+
+      "text_5": {
+        "type": "text",
+        "id": "text_5",
+        "content": 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus quis scelerisque ligula. Proin tristique ligula id magna finibus rhoncus. Quisque dictum viverra sapien, vel elementum metus condimentum nec. Donec ac tellus nunc. Nullam fermentum pharetra justo, accumsan tristique quam tempus a. Quisque vitae luctus velit. Praesent lacinia enim ex, sed pulvinar neque dictum ultricies. Sed est metus, bibendum sed suscipit ut, cursus ut mi. Pellentesque sagittis mi nisi, eu blandit metus congue id. Pellentesque eget magna porta, rutrum odio et, commodo lacus. Sed vitae vehicula ante. Quisque suscipit iaculis est, vitae aliquet lacus dictum ut. Nulla enim dolor, pulvinar at odio vitae, sollicitudin eleifend ex. Maecenas eget ligula eget sem efficitur consectetur nec vel sem. In massa mauris, consequat vitae enim eget, vehicula aliquet turpis.'
       }
     }
   };
