ddec47b14b4b7f23bb0f41cc2e373ea862eea004
Daniel Beilinson <beilinson@gmail.com>
2015-04-20 21:47:46 +0200

    search api suggestions

--- controllers/search.js
+++ controllers/search.js
@@ -8,27 +8,51 @@ var Location = require('../models/location.js')
 
 
 var search = function(query, cb) {
-  var type = query.type || false;
+  var type = query.type || false,
+      result = [];
   if (!query.type) {
-    plainSearch(query, cb);
+    plainSearch(query, function(err, searchResult){
+      var total = searchResult.length;
+      if (total == 0) {
+        plainSearch('', function(err, res) {
+          var total = searchResult.length;
+          result.push({ state: "We didn't found anything matching your query, but here are our suggestions" });
+          result.push(res);
+          cb(null, result);
+        })
+      } else {
+        result.push({ state: total + ' items matching your query' });
+        result.push(searchResult);
+        cb(null, result);
+      }
+    });
   } else {
     switch (type) {
       case 'location':
         Location.search(query, function (err, output) {
           if (err) return callback(err);
-          cb(null, output);
+          var total = output.length;
+          result.push({ state: total + ' locations matching your query' });
+          result.push(output);
+          cb(null, result);
         });
         break;
       case 'person':
         Person.search(query, function (err, output) {
           if (err) return callback(err);
-          cb(null, output);
+          var total = output.length;
+          result.push({ state: total + ' persons matching your query' });
+          result.push(output);
+          cb(null, result);
         });
         break;
       case 'definition':
         Definition.search(query, function (err, output) {
           if (err) return callback(err);
-          cb(null, output);
+          var total = output.length;
+          result.push({ state: total + ' definitions matching your query' });
+          result.push(output);
+          cb(null, result);
         });
         break;
       default:
