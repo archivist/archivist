93390fd34617ec786b17b22bcc04c36951685f37
Daniel Beilinson <beilinson@gmail.com>
2015-07-04 20:07:53 +0300

    persons report

--- controllers/import/remarks.js
+++ controllers/import/remarks.js
@@ -109,7 +109,7 @@ exports.writeOutPersonsReport = function(doc, report, timecodesMap, intro) {
 			content += '- ';
 			content += item.person.values.join(', ');
 			content += " (" + item.person.id + ");\n";
-			console.log('startCode',startCode)
+			console.log('startCode',item.timecodes[0])
 			findOrCreatePersonReport(doc, content, intro, startCode, endCode);
 		}
 	});
