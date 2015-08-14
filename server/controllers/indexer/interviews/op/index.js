29290ddaf5ec5305c24643be48e021251de70115
Daniel Beilinson <beilinson@gmail.com>
2015-08-14 14:43:35 +0300

    index interviews api prortotype

--- server/controllers/indexer/interviews/op/index.js
+++ server/controllers/indexer/interviews/op/index.js
@@ -2,18 +2,53 @@ var Interview = require('archivist-core/interview');
 var indexInterview = require('./index_interview.js');
 var deleteInterview = require('./delete_interview.js');
 var _ = require('underscore');
-var request = require('superagent');
 var elasticsearch = require('elasticsearch');
 var config = require('../../config');
 
-var getJSON = function(url, cb) {
-  request
-    .get(url)
-    .end(function(err, res){
-      if(err) console.error(err);
-      cb(err, res.body);
+/**
+ * Add Interview and all related fragments to index
+ *
+ * @param {string} id - Id of interview to add
+ * @param {function} cb - The callback that handles the results 
+ */
+
+var addToIndex = function(id, cb) {
+  var client = new elasticsearch.Client(_.clone(config));
+
+  Document.getCleaned(id, false, function(err, json){
+    if (err) return cb(err);
+    console.log('Indexing interview %s...', id);
+    var interview = new Interview.fromJson(json);
+    indexInterview(client, interview, function(err) {
+      if (err) {
+        console.error(err);
+      } else {
+        console.log('Done.');
+      }
+      client.close();
+      cb(err);
     });
+  });
 }
+
+/**
+ * Update Interview record inside index
+ *
+ * @param {string} id - Id of interview to update
+ * @param {boolean} meta - Meta mode to update only Interview metadata, otherwise fragments will also updated 
+ * @param {function} cb - The callback that handles the results 
+ */
+
+var updateIndex = function(id, meta, cb) {
+  
+}
+
+/**
+ * Removes Interview and all related fragments from index
+ *
+ * @param {string} id - Id of interview to remove
+ * @param {function} cb - The callback that handles the results 
+ */
 
 var deleteFromIndex = function(id, cb) {
   var client = new elasticsearch.Client(_.clone(config));
@@ -32,29 +67,6 @@ var deleteFromIndex = function(id, cb) {
       client.close();
       console.log("Interview", id, "has been removed from index.");
       cb(null);
-    });
-  });
-}
-
-var updateIndex = function(id, cb) {
-  var interviewUrl = config.archive + '/api/documents/' + id;
-  var client = new elasticsearch.Client(_.clone(config));
-
-  deleteFromIndex(id, function(err){
-    if (err) return cb(err);
-    getJSON(interviewUrl, function(err, json){
-      if (err) return cb(err);
-      console.log('Indexing interview %s...', interviewUrl);
-      var interview = new Interview.fromJson(json);
-      indexInterview(client, interview, function(err) {
-        if (err) {
-          console.error(err);
-        } else {
-          console.log('Done.');
-        }
-        client.close();
-        cb(err);
-      });
     });
   });
 }
