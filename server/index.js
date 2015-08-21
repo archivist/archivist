eafd3470d096b76f4a30e0053c3e586e18c81f9f
Daniel Beilinson <beilinson@gmail.com>
2015-08-21 21:38:25 +0300

    make mongo replica optional

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ var browserify = require('browserify');
 // MONGOOSE CONNECT
 
 var replica_name = process.env.RS_NAME
-	,	replica_url = process.env.MONGO_URL;
+	,	mongo_url = process.env.MONGO_URL;
 
 try {
 	var options = {
@@ -31,16 +31,18 @@ try {
 				keepAlive: 1,
 				connectTimeoutMS: 30000
 			}
-		},
-		replset: {
+		}
+	};
+	if(replica_name) {
+		options.replset = {
 			rs_name: replica_name,
 			socketOptions : {
 				keepAlive : 1,
 				connectTimeoutMS: 30000
 			}
 		}
-	};
-	mongoose.connect(replica_url, options);
+	}
+	mongoose.connect(mongo_url, options);
 	console.log("Started connection, waiting for it to open...");
 } catch (err) {
 	console.log(("Setting up failed to connect"), err.message);
