40082193cd3aec0d92d76137c5a230213cc6863f
Daniel Beilinson <beilinson@gmail.com>
2016-02-05 15:57:19 +0300

    ga code

--- src/memorial/ga.js
+++ src/memorial/ga.js
@@ -3,5 +3,5 @@
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-ga('create', 'UA-41907823-3', 'auto');
+ga('create', 'UA-41907823-4', 'auto');
 ga('send', 'pageview');
