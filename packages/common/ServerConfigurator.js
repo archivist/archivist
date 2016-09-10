9113eda80282348e097058a4ee5f6084205f1ff5
Daniel Beilinson <beilinson@gmail.com>
2016-09-10 16:37:38 +0300

    Server side configurator.

---
+++ packages/common/ServerConfigurator.js
@@ -0,0 +1,93 @@
+'use strict';
+
+var Configurator = require('substance/util/Configurator');
+
+function ServerConfigurator() {
+  ServerConfigurator.super.apply(this, arguments);
+
+  // Extend config
+  this.config.stores = {};
+  this.config.engines = {};
+}
+
+ServerConfigurator.Prototype = function() {
+
+  /*
+    Set app config
+  */
+  this.setAppConfig = function(config) {
+    this.config.app = config;
+  };
+
+  /*
+    Get app config
+  */
+  this.getAppConfig = function() {
+    return this.config.app;
+  };
+
+  this.setServerApp = function(app) {
+    this.config.server = app;
+  };
+
+  this.getServerApp = function() {
+    return this.config.server;
+  };
+
+  this.setWebSocketServer = function(wss) {
+    this.config.wss = wss;
+  };
+
+  this.getWebSocketServer = function() {
+    return this.config.wss;
+  };
+
+  /*
+    Set database connection
+  */
+  this.setDBConnection = function(db) {
+    this.config.db = db;
+  };
+
+  /*
+    Get database connection
+  */
+  this.getDBConnection = function() {
+    return this.config.db.connection;
+  };
+
+  /*
+    Add store
+  */
+  this.addStore = function(name, StoreClass) {
+    this.config.stores[name] = StoreClass;
+  };
+
+  /*
+    Get store
+  */
+  this.getStore = function(name) {
+    var db = this.getDBConnection();
+    var StoreClass = this.config.stores[name];
+    return new StoreClass({db: db});
+  };
+
+  /*
+    Add engine
+  */
+  this.addEngine = function(name, engineInstance, config) {
+    this.config.engines[name] = engineInstance;
+  };
+
+  /*
+    Get engine
+  */
+  this.getEngine = function(name) {
+    return this.config.engines[name];
+  };
+
+};
+
+Configurator.extend(ServerConfigurator);
+
+module.exports = ServerConfigurator;
