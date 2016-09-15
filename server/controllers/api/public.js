6b7bc20154f6db73a59bd1a303ef8931f64ff24b
Daniel Beilinson <beilinson@gmail.com>
2016-09-16 00:01:06 +0300

    Console found entities.

--- server/controllers/api/public.js
+++ server/controllers/api/public.js
@@ -128,6 +128,7 @@ var getPublishedLocations = function(cb) {
   }
   interviews.countEntities(options, function(err, counters){
     var entities = _.keys(counters);
+    console.log(entities);
     var query = {
       _id: {
         '$in': entities
