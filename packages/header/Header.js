6fb65b1110ba94ccb951195f2e37daf5354cbd1d
Daniel Beilinson <beilinson@gmail.com>
2016-11-01 17:20:39 +0300

    Header package.

--- packages/header/Header.js
+++ packages/header/Header.js
@@ -1,48 +1,41 @@
-'use strict';
-
 import { Component } from 'substance'
 import forEach from 'lodash/forEach'
 
-function Header() {
-  Component.apply(this, arguments);
-}
+class Header extends Component {
 
-Header.Prototype = function() {
+  render($$) {
+    let configurator = this.context.configurator
+    let authenticationClient = this.context.authenticationClient
+    // var componentRegistry = this.context.componentRegistry;
+    // var LoginStatus = componentRegistry.get('login-status');
 
-  this.render = function($$) {
-    var configurator = this.context.configurator;
-    var authenticationClient = this.context.authenticationClient;
-    var componentRegistry = this.context.componentRegistry;
-    var LoginStatus = componentRegistry.get('login-status');
+    let el = $$('div').addClass('sc-header')
+    let actionEls = []
 
-    var el = $$('div').addClass('sc-header');
-    var actionEls = [];
-
-    var MenuItems = configurator.getMenuItems();
+    let MenuItems = configurator.getMenuItems()
     forEach(MenuItems, function(item) {
       actionEls.push(
         $$('button').addClass('se-action')
           .append(item.label)
           .on('click', this.send.bind(this, 'navigate', {page: item.action}))
-      );
-    }.bind(this));
+      )
+    }.bind(this))
 
-    var content = [];
+    let content = []
     if (this.props.content) {
-      content = content.concat(this.props.content);
+      content = content.concat(this.props.content)
     }
 
     el.append(
+      $$('div').addClass('se-icon'),
       $$('div').addClass('se-actions').append(actionEls),
       $$('div').addClass('se-content').append(content)
       // $$(LoginStatus, {
       //   user: authenticationClient.getUser()
       // })
-    );
-    return el;
-  };
-};
+    )
+    return el
+  }
+}
 
-Component.extend(Header);
-
-export default Header;
+export default Header
--- packages/header/package.js
+++ packages/header/package.js
@@ -1,11 +1,8 @@
-'use strict';
-
 import Header from './Header'
 
 export default {
   name: 'archivist-header',
   configure: function(config) {
-    config.addComponent('header', Header);
-    //config.addStyle(__dirname, '_header');
+    config.addComponent('header', Header)
   }
-};
+}
