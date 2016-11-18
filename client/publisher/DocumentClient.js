8a0f17c379fa4f83d4068f4a90a0794dfe3bf28e
Daniel Beilinson <beilinson@gmail.com>
2016-11-18 23:23:05 +0300

    Document Client improvements.

--- client/publisher/DocumentClient.js
+++ client/publisher/DocumentClient.js
@@ -1,6 +1,4 @@
-'use strict';
-
-import { request, DocumentClient } from 'substance';
+import { request, DocumentClient } from 'substance'
 
 /*
   HTTP client for talking with DocumentServer
@@ -8,24 +6,28 @@ import { request, DocumentClient } from 'substance';
 
 class ArchivistDocumentClient extends DocumentClient {
 
-  listDocuments(cb) {
-    request('GET', '/api/documents', null, cb);
+  listDocuments(filters, options, cb) {
+    let filtersRequest = encodeURIComponent(JSON.stringify(filters))
+    let optionsRequest = encodeURIComponent(JSON.stringify(options))
+    request('GET', '/api/documents?&filters=' + filtersRequest + '&options=' + optionsRequest, null, cb)
+  }
+
+  getReferences(resourceId, filters, options, cb) {
+    filters['annotations @>'] = [resourceId]
+    return this.listDocuments(filters, options, cb)
   }
 
   searchDocuments(query, language, filters, options, cb) {
-    // TODO: send filters and options to server
-    let filtersRequest = encodeURIComponent(JSON.stringify(filters));
-    let optionsRequest = encodeURIComponent(JSON.stringify(options));
+    let filtersRequest = encodeURIComponent(JSON.stringify(filters))
+    let optionsRequest = encodeURIComponent(JSON.stringify(options))
     request('GET', '/api/documents/search?query=' + query + '&language=' + language + '&filters=' + filtersRequest + '&options=' + optionsRequest, null, cb)
   }
 
   searchFragments(documentId, query, language, filters, options, cb) {
-    // TODO: send filters and options to server
-    let filtersRequest = encodeURIComponent(JSON.stringify(filters));
-    let optionsRequest = encodeURIComponent(JSON.stringify(options));
+    let filtersRequest = encodeURIComponent(JSON.stringify(filters))
+    let optionsRequest = encodeURIComponent(JSON.stringify(options))
     request('GET', '/api/documents/' + documentId + '/search?query=' + query + '&language=' + language + '&filters=' + filtersRequest + '&options=' + optionsRequest, null, cb)
   }
-
 }
 
 export default ArchivistDocumentClient
