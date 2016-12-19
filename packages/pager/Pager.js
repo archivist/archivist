2fa5b844545dcd8c5bb6b4c7724f782bd159411b
Daniel Beilinson <beilinson@gmail.com>
2016-12-19 15:20:55 +0300

    Introduce pager.

---
+++ packages/pager/Pager.js
@@ -0,0 +1,25 @@
+import { Button, Component } from 'substance'
+
+class Pager extends Component {
+
+  render($$) {
+    let total = this.props.total;
+    let loaded = this.props.loaded;
+    let isLastPage = loaded >= total;
+
+    let el = $$('div').addClass('sc-pager');
+
+    let btn = $$(Button, {disabled: isLastPage, label: 'Load more', style: 'outline'})
+      .on('click', this._loadMore)
+
+    el.append(btn)
+
+    return el
+  }
+
+  _loadMore() {
+    this.send('loadMore')
+  }
+}
+
+export default Pager
---
+++ packages/pager/package.js
@@ -0,0 +1,12 @@
+import Pager from './Pager'
+
+export default {
+  name: 'pager',
+  configure: function(config) {
+    config.addComponent('pager', Pager)
+
+    config.addLabel('load-more', {
+      en: 'Load more'
+    })
+  }
+}
