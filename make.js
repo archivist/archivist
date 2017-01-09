c96470b25661a6cff79198e066aee051032cc4b2
Daniel Beilinson <beilinson@gmail.com>
2017-01-09 15:08:29 +0300

    Build multiple clients.

--- make.js
+++ make.js
@@ -17,25 +17,27 @@ b.task('substance', function() {
   b.copy('node_modules/substance/dist', './dist/substance')
 })
 
-function buildApp(app) {
+function buildApp(app, core) {
   return function() {
-    _archvistJS()
+    if(core) {
+      _archvistJS()
+      b.css('./node_modules/substance/dist/substance-pagestyle.css', 'dist/archivist/archivist-pagestyle.css', {variables: true})
+      b.css('./node_modules/substance/dist/substance-reset.css', 'dist/archivist/archivist-reset.css', {variables: true})
+    }
     b.copy('client/'+app+'/index.html', './dist/'+app+'/')
     b.copy('client/'+app+'/assets', './dist/'+app+'/assets/')
     b.css('./client/' + app + '/app.css', 'dist/' + app + '/' + app + '.css', {variables: true})
-    b.css('./node_modules/substance/dist/substance-pagestyle.css', 'dist/archivist-pagestyle.css', {variables: true})
-    b.css('./node_modules/substance/dist/substance-reset.css', 'dist/archivist-reset.css', {variables: true})
     b.js('client/' + app + '/app.js', {
       // need buble if we want to minify later
       buble: true,
       external: ['substance'],
       commonjs: { include: ['node_modules/lodash/**', 'node_modules/moment/moment.js'] },
-      dest: './dist/app.js',
+      dest: './dist/' + app + '/app.js',
       format: 'umd',
       moduleName: app
     })
     b.custom('injecting config', {
-      src: './dist/app.js',
+      src: './dist/' + app + '/app.js',
       dest: './dist/' + app + '/' + app + '.js',
       execute: function(file) {
         const code = fs.readFileSync(file[0], 'utf8')
@@ -43,9 +45,9 @@ function buildApp(app) {
         fs.writeFileSync(this.outputs[0], result, 'utf8')
       }      
     })
-    b.copy('./dist/app.js.map', './dist/' + app + '/' + app + '.js.map')
-    b.rm('./dist/app.js')
-    b.rm('./dist/app.js.map')
+    b.copy('./dist/' + app + '/app.js.map', './dist/' + app + '/' + app + '.js.map')
+    b.rm('./dist/' + app + '/app.js')
+    b.rm('./dist/' + app + '/app.js.map')
   }
 }
 
@@ -55,15 +57,16 @@ function _archvistJS() {
     external: ['substance'],
     commonjs: { include: ['node_modules/lodash/**'] },
     targets: [{
-      dest: 'dist/archivist.cjs.js',
+      dest: 'dist/archivist/archivist.cjs.js',
       format: 'cjs', sourceMapRoot: __dirname, sourceMapPrefix: 'archivist'
     }]
   })
 }
 
-b.task('publisher', ['clean', 'substance', 'assets'], buildApp('publisher'))
+b.task('publisher', ['clean', 'substance', 'assets'], buildApp('publisher', true))
+b.task('scholar', buildApp('scholar'))
 
-b.task('client', ['publisher'])
+b.task('client', ['publisher', 'scholar'])
 
 // build all
 b.task('default', ['client'])
