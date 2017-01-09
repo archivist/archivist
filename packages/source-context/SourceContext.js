30b8ac1deaab662625be15bf06e751d6b7e0d9a6
Daniel Beilinson <beilinson@gmail.com>
2017-01-10 02:42:10 +0300

    Setup video player.

--- packages/source-context/SourceContext.js
+++ packages/source-context/SourceContext.js
@@ -1,12 +1,62 @@
 import { Component } from 'substance'
+import plyr from 'plyr'
 
 class SourceContext extends Component {
+
+  didMount() {
+    plyr.setup()
+  }
+
+  didUpdate() {
+    let time = this.props.time
+    if(time) {
+      let player = plyr.get()
+      player = player[0]
+      if(player) {
+        let seconds = this.hmsToSecondsOnly(time)
+        player.seek(seconds)
+      }
+    }
+  }
+
+  getInitialState() {
+    let doc = this.context.doc
+    let meta = doc.getDocumentMeta()
+
+    return {
+      type: meta.record_type,
+      media: meta.media_id
+    }
+  }
+
+  shouldRerender(newProps, newState) { // eslint-disable-line
+    return false
+  }
+
   render($$) {
     let el = $$('div').addClass('sc-context-panel')
 
-    el.append('SOURCE')
+    if(this.state.type === 'video') {
+      //<div data-type="youtube" data-video-id="bTqVqk7FSmY"></div
+      let player = $$('div')
+        .addClass('se-media')
+        .attr({'data-type': 'vimeo', 'data-video-id': this.state.media})
+
+      el.append(player)
+    }
 
     return el
+  }
+
+  hmsToSecondsOnly(str) {
+    let p = str.split(':'), s = 0, m = 1
+
+    while (p.length > 0) {
+      s += m * parseInt(p.pop(), 10)
+      m *= 60
+    }
+
+    return s
   }
 }
 
