b123c1852e2573efcfb598e9da91bb925d820a77
Daniel Beilinson <beilinson@gmail.com>
2017-01-13 02:58:36 +0300

    Toponym context item.

--- packages/toponym/ToponymContextItem.js
+++ packages/toponym/ToponymContextItem.js
@@ -3,14 +3,44 @@ import { Component } from 'substance'
 class ToponymContextItem extends Component {
 
   render($$) {
+    let urlHelper = this.context.urlHelper
+    let node = this.props.data
+
     let el = $$('div')
       .attr("data-id", this.props.entityId)
       .addClass('sc-entity-entry se-toponym')
       .on('click', this.handleClick)
-      .append(
-        $$('div').addClass('se-title').append(this.props.data.name),
-        $$('div').addClass('se-description').setInnerHTML(this.props.data.description)
-      )
+
+    let location = node.country
+    if(node.name !== node.currentName && node.currentName) {
+      location += ", " + node.currentName
+    }
+
+    let resourceLink = $$('a')
+      .addClass('se-resource-external-link se-resource-link')
+      .attr({
+        href: urlHelper.openResource(this.props.entityId),
+        target: '_blank',
+        title: this.getLabel('resource-link')
+      })
+      .append(this.context.iconProvider.renderIcon($$, 'resources'))
+
+    let mapLink = $$('a')
+      .addClass('se-resource-external-link se-map-link')
+      .attr({
+        href: urlHelper.openResource(this.props.entityId),
+        target: '_blank',
+        title: this.getLabel('map-link')
+      })
+      .append(this.context.iconProvider.renderIcon($$, 'map-link'))
+
+    el.append(
+      $$('div').addClass('se-title').append(node.name),
+      $$('div').addClass('se-location').append(location),
+      resourceLink,
+      mapLink,
+      $$('div').addClass('se-description').setInnerHTML(node.description)
+    )
 
     return el
   }
