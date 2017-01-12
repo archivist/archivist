e9b783f95370635642a2d094f6479cdeb9af193c
Daniel Beilinson <beilinson@gmail.com>
2017-01-13 02:59:03 +0300

    Definition context item.

--- packages/definition/DefinitionContextItem.js
+++ packages/definition/DefinitionContextItem.js
@@ -3,14 +3,28 @@ import { Component } from 'substance'
 class DefinitionContextItem extends Component {
 
   render($$) {
+    let urlHelper = this.context.urlHelper
+    let node = this.props.data
+
     let el = $$('div')
       .attr("data-id", this.props.entityId)
       .addClass('sc-entity-entry se-definition')
       .on('click', this.handleClick)
-      .append(
-        $$('div').addClass('se-title').append(this.props.data.title),
-        $$('div').addClass('se-description').setInnerHTML(this.props.data.description)
-      )
+
+    let resourceLink = $$('a')
+      .addClass('se-resource-external-link se-resource-link')
+      .attr({
+        href: urlHelper.openResource(this.props.entityId),
+        target: '_blank',
+        title: this.getLabel('resource-link')
+      })
+      .append(this.context.iconProvider.renderIcon($$, 'resources'))
+
+    el.append(
+      resourceLink,
+      $$('div').addClass('se-title').append(node.name),
+      $$('div').addClass('se-description').setInnerHTML(node.description)
+    )
 
     return el
   }
