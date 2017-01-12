a39a0c49dd29d57cb350d26c55e874e153e741c4
Daniel Beilinson <beilinson@gmail.com>
2017-01-13 02:58:52 +0300

    Person context item.

--- packages/person/PersonContextItem.js
+++ packages/person/PersonContextItem.js
@@ -3,14 +3,31 @@ import { Component } from 'substance'
 class PersonContextItem extends Component {
 
   render($$) {
+    let urlHelper = this.context.urlHelper
+    let node = this.props.data
+
     let el = $$('div')
       .attr("data-id", this.props.entityId)
       .addClass('sc-entity-entry se-person')
       .on('click', this.handleClick)
-      .append(
-        $$('div').addClass('se-title').append(this.props.data.name),
-        $$('div').addClass('se-description').setInnerHTML(this.props.data.description)
-      )
+
+    let resourceLink = $$('a')
+      .addClass('se-resource-external-link se-person-link')
+      .attr({
+        href: urlHelper.openPerson(this.props.entityId),
+        target: '_blank',
+        title: this.getLabel('person-link')
+      })
+      .append(this.context.iconProvider.renderIcon($$, 'person-link'))
+
+    if(node.global) {
+      el.append(resourceLink)
+    }
+
+    el.append(
+      $$('div').addClass('se-title').append(node.name),
+      $$('div').addClass('se-description').setInnerHTML(node.description)
+    )
 
     return el
   }
