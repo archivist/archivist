2222162a0d2df63a53dd9f32224d13cb5726c220
Daniel Beilinson <beilinson@gmail.com>
2017-01-18 13:33:58 +0300

    Expose engines and servers as es6 modules.

--- server.es.js
+++ server.es.js
@@ -13,18 +13,26 @@ export { default as ServerConfigurator } from './packages/common/ServerConfigura
 // Engine
 export { default as ArchivistEnginePackage } from './packages/engine/package'
 export { default as AuthEnginePackage } from './packages/engine/auth/package'
+export { default as AuthEngine } from './packages/engine/auth/AuthEngine'
 export { default as DocumentEnginePackage } from './packages/engine/document/package'
+export { default as DocumentEngine } from './packages/engine/document/DocumentEngine'
 //export { default as MailerEnginePackage } from './packages/engine/mailer/package'
 export { default as ResourceEnginePackage } from './packages/engine/resource/package'
+export { default as ResourceEngine } from './packages/engine/resource/ResourceEngine'
 export { default as SnapshotEnginePackage } from './packages/engine/snapshot/package'
 
 // Server
 //export { default as ArchivistServerPackage } from './packages/server/package'
 export { default as AuthServerPackage } from './packages/server/auth/package'
+export { default as AuthServer } from './packages/server/auth/AuthServer'
 export { default as CollabServerPackage } from './packages/server/collab/package'
+export { default as CollabServer } from './packages/server/collab/CollabServer'
 export { default as DocumentServerPackage } from './packages/server/document/package'
+export { default as DocumentServer } from './packages/server/document/DocumentServer'
 export { default as ResourceServerPackage } from './packages/server/resource/package'
+export { default as ResourceServer } from './packages/server/resource/ResourceServer'
 export { default as UserServerPackage } from './packages/server/user/package'
+export { default as UserServer } from './packages/server/user/UserServer'
 
 // Store
 export { default as ArchivistStorePackage } from './packages/store/package'
