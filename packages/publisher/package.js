a9789d2078e37680b50ad524077f3ba6445526cd
Daniel Beilinson <beilinson@gmail.com>
2017-03-15 19:29:43 +0300

    Repair publisher.

--- packages/publisher/package.js
+++ packages/publisher/package.js
@@ -1,6 +1,4 @@
 import { BasePackage, PersistencePackage } from 'substance'
-import InterviewPackage from '../interview/package'
-import EntityPackage from '../entity/package'
 import PublisherLayout from './PublisherLayout'
 
 export default {
@@ -9,11 +7,6 @@ export default {
     // Now import base packages
     config.import(BasePackage)
     config.import(PersistencePackage)
-
-    config.import(InterviewPackage)
-
-    config.import(EntityPackage)
-
     config.addComponent('editor', PublisherLayout)
   }
 }
