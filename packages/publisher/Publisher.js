8643b115e5f9de8a534d3cf415f7c8d32de61117
Daniel Beilinson <beilinson@gmail.com>
2017-05-28 12:43:04 +0300

    Publisher highlighting and focusing improvements.

--- packages/publisher/Publisher.js
+++ packages/publisher/Publisher.js
@@ -1,5 +1,5 @@
 import { ContainerEditor, Highlights, Layout, ProseEditor, SplitPane, Toolbar } from 'substance'
-import { forEach } from 'lodash-es'
+import { forEach, map, orderBy, uniq } from 'lodash-es'
 import PublisherContext from './PublisherContext'
 
 class Publisher extends ProseEditor {
@@ -10,6 +10,7 @@ class Publisher extends ProseEditor {
     this.contentHighlights = new Highlights(doc)
 
     this.handleActions({
+      'showReferences': this._showReferences,
       'toggleBracket': this._toggleBracket
     })
   }
@@ -19,6 +20,7 @@ class Publisher extends ProseEditor {
     let editorSession = this.getEditorSession()
     editorSession.onUpdate(this._onSessionUpdate, this)
     editorSession.on('createInlineEntityReference', this._createEntityReference, this)
+    editorSession.on('createComment', this._createComment, this)
   }
 
   dispose() {
@@ -104,6 +106,60 @@ class Publisher extends ProseEditor {
     return contentPanel
   }
 
+  highlightReferences(entities, containerAnnos) {
+    let editorSession = this.editorSession
+    let doc = editorSession.getDocument()
+    let entityIndex = doc.getIndex('entities')
+    let schema = doc.getSchema()
+    let nodes = schema.nodeRegistry.entries
+    let highlights = {}
+    forEach(nodes, node => {
+      if(node.schema.hasOwnProperty('reference')) {
+        highlights[node.type] = []
+      }
+    })
+    forEach(entities, entityId => {
+      if(containerAnnos) {
+        let refs = entityIndex.get(entityId)
+        forEach(refs, ref => {
+          let entityType = ref.type
+          highlights[entityType] = highlights[entityType].concat(ref.id + '-bracket')
+          highlights[entityType] = uniq(highlights[entityType])
+        })
+      } else {
+        let refs = entityIndex.get(entityId)
+        let keys = Object.keys(refs)
+        if(keys.length > 0) {
+          let entityType = refs[keys[0]].type
+          let annos = map(refs, n => {return n.id})
+          highlights[entityType] = highlights[entityType].concat(annos)
+        }
+      }
+    })
+
+    this.contentHighlights.set(highlights)
+  }
+
+  _showReferences(entityId, silent) {
+    let container = this.refs.body.getContainer()
+    let editorSession = this.editorSession
+    let doc = editorSession.getDocument()
+    let entityIndex = doc.getIndex('entities')
+    let refs = entityIndex.get(entityId)
+    let ordered = orderBy(refs, ref => {
+      let p = ref.path[0]
+      return container.getPosition(p)
+    })
+
+    this.refs.contentPanel.scrollTo(ordered[0].id)
+    this.highlightReferences([entityId])
+
+    // if(!silent) {
+    //   let urlHelper = this.context.urlHelper
+    //   urlHelper.focusResource(entityId)
+    // }
+  }
+
   _onSessionUpdate(editorSession) {
     if (!editorSession.hasChanged('document') && !editorSession.hasChanged('selection')) return
 
@@ -183,13 +239,17 @@ class Publisher extends ProseEditor {
     let highlights = {}
     let highlighted = active ? [active] : []
     highlights[node.type] = highlighted
-
     this.contentHighlights.set(highlights)
   }
 
   _createEntityReference() {
     let contextPanel = this.refs.contextPanel
     contextPanel.editResource()
+  }
+
+  _createComment(anno) {
+    let contextPanel = this.refs.contextPanel
+    contextPanel.editComment(anno)
   }
 
 }
