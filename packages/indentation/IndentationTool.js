f2c79c938a3157373eb618c3010190db6b48a559
Daniel Beilinson <beilinson@gmail.com>
2017-07-31 23:10:34 +0300

    Upgrade Indentation package.

--- packages/indentation/IndentationTool.js
+++
@@ -1,5 +0,0 @@
-import { Tool } from 'substance'
-
-class IndentationTool extends Tool {}
-
-export default IndentationTool
--- packages/indentation/package.js
+++ packages/indentation/package.js
@@ -1,11 +1,9 @@
-import IndentationTool from './IndentationTool'
 import IndentationCommand from './IndentationCommand'
 
 export default {
   name: 'indentation-cleaner',
   configure: function(config) {
-    config.addTool('indentation-cleaner', IndentationTool, {toolGroup: 'utils'})
-    config.addCommand('indentation-cleaner', IndentationCommand)
+    config.addCommand('indentation-cleaner', IndentationCommand, { commandGroup: 'utils' })
     config.addIcon('indentation-cleaner', {'fontawesome': 'fa-dedent'})
   }
 }
