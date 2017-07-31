8fa6d6f9d53b680dee66ef41a00e344936321288
Daniel Beilinson <beilinson@gmail.com>
2017-07-31 23:12:08 +0300

    Upgrade Whitespace package.

--- packages/whitespace/WhitespaceTool.js
+++
@@ -1,5 +0,0 @@
-import { Tool } from 'substance'
-
-class WhitespaceTool extends Tool {}
-
-export default WhitespaceTool
--- packages/whitespace/package.js
+++ packages/whitespace/package.js
@@ -1,11 +1,9 @@
-import WhitespaceTool from './WhitespaceTool'
 import WhitespaceCommand from './WhitespaceCommand'
 
 export default {
   name: 'whitespace-cleaner',
   configure: function(config) {
-    config.addTool('whitespace-cleaner', WhitespaceTool, {toolGroup: 'utils'})
-    config.addCommand('whitespace-cleaner', WhitespaceCommand)
+    config.addCommand('whitespace-cleaner', WhitespaceCommand, { commandGroup: 'utils' })
     config.addIcon('whitespace-cleaner', {'fontawesome': 'fa-eraser'})
   }
 }
